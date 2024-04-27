11ddea7e502c396c03be2985e8d207af46253101
James <Jamestngo02@gmail.com>
2024-04-27 15:14:02 -0400

    adding the connection object and testing the connection

--- project/src/finalProjectMain.java
+++ project/src/finalProjectMain.java
@@ -1,5 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class finalProjectMain {
     public static void main(String[] args) {
+		//protocol , ip address ,and then the port 
+		String jdbcConn = "jdbc:mysql://localhost:3306/finalguiproject?";
+		String uname = "guest";
+		String password = "guestguest";
+
+		//connection obect
+		try {
+			Connection con = DriverManager.getConnection(jdbcConn, uname, password);
+
+			System.out.println("connection worked");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		FinalProjectGUI frame = new FinalProjectGUI();
 		frame.setVisible(true);

d5db33a63170e62b9c65536340d8b53d5da31565
James <Jamestngo02@gmail.com>
2024-04-28 12:25:48 -0400

    removing comments from the main method

--- project/src/finalProjectMain.java
+++ project/src/finalProjectMain.java
@@ -6,31 +6,9 @@ import java.sql.SQLException;
 public class finalProjectMain {
     public static void main(String[] args) {
 		//protocol , ip address ,and then the port 
-		String jdbcConn = "jdbc:mysql://localhost:3306/finalguiproject?";
-		String uname = "guest";
-		String password = "guest";
-
-		//connection obect
-		// try {
-        //     System.out.println("connected??");
-		// 	Connection con = DriverManager.getConnection(jdbcConn, uname, password);
-
-		// 	System.out.println("connection worked");
-
-		// 	String query = "insert into product(prodName, price) values('rice', 1.99)";
-
-		// 	java.sql.Statement st = con.createStatement();
-		// 	st.execute(query);
-
-            
-			
-
-
-
-		// } catch (SQLException e) {
-		// 	// TODO Auto-generated catch block
-		// 	e.printStackTrace();
-		// }
+		// String jdbcConn = "jdbc:mysql://localhost:3306/finalguiproject?";
+		// String uname = "guest";
+		// String password = "guest";
 
         //open execute close . open execute close 
 
